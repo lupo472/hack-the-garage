aaca8fb7d0f7cfd3124f10037c94666354ae2756
Fabio Brunori <fabio.brunori.94@gmail.com>
2017-02-23 15:26:53 +0100

    added example testing

--- src/app/1st.spec.ts
+++ src/app/1st.spec.ts
@@ -1,3 +1,38 @@
-describe('1st tests', () => {
-    it('true is true', () => expect(true).toBe(true));
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
+import { ListComponent } from './page_components/list.component';
+
+describe("List page Component (template)", () => {
+    let comp:    ListComponent;
+    let fixture: ComponentFixture<ListComponent>;
+    let de:      DebugElement;
+    let el:      HTMLElement;
+
+    //beforeEach permette a TestBed di resettarsi prima di ogni test, evitando cazzi amari
+    beforeEach(async(() => {
+        //creo un modulo fittizio per contenere gli oggetti che testo
+        TestBed.configureTestingModule({
+            declarations: [ ListComponent ],
+        })
+        .compileComponents().catch((m)=> {
+            console.log(m);
+        });
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ListComponent);
+
+        comp = fixture.componentInstance;
+
+        de = fixture.debugElement.query(By.css("div#array"));
+        el = de.nativeElement;
+
+    });
+
+    it('should display the json Object', () => {
+        fixture.detectChanges();
+        expect(el.textContent).toBeDefined();
+    });
 });
--- src/app/page_components/list.component.ts
+++ src/app/page_components/list.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component( {
-    selector: "list-page",
-    templateUrl: "/app/pages/list.template.html"
+    selector: 'list-page',
+    templateUrl: '/app/pages/list.template.html'
 })
 export class ListComponent {
      items = [{name: "Item1"}, {name: "Item2"}, {name: "Your Sister"}];
