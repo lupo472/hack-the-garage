98aa5a414852d6c4d2994aa88441b120de3f7864
lorenzovalente <lorenzo.valente.91@gmail.com>
2017-02-23 20:19:48 +0100

    no message

--- src/app/page_components/pdf.component.ts
+++ src/app/page_components/pdf.component.ts
@@ -9,15 +9,22 @@ import {Component} from "@angular/core";
 export class DocSection {
 
     constructor() {
+
     }
 
     public download() {
 
         var doc = new jsPDF();
-        doc.text(20, 20, 'Hello world!');
-        doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
-        doc.addPage();
-        doc.text(20, 20, 'Do you like that?');
+        // doc.text(20, 20, 'Hello world!');
+        // doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.');
+        // doc.addPage();
+        // doc.text(20, 20, 'Do you like that?');
+        doc.fromHTML('<h2>Applicant Master Data</h2>' +
+            ' <p> This is a hindi developer </p> ' +
+            '<h2>Company Master Data</h2> ' +
+            '<p>This is hindi company</p> ' +
+            '<h2>Business Plan</h2> ' +
+            '<p>Hindi freelancing</p>');
 
         // Save the PDF
         doc.save('Test.pdf');
