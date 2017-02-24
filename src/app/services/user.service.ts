2796c2c40669002545cbede1b8db2e12acd43e19
luca <luca.antilici@gmail.com>
2017-02-24 23:28:56 +0100

    Added login and signup api and services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,9 +5,17 @@ import { ApiService } from './api.service'
 @Injectable()
 export class UserService{
 
-    constructor(backend: ApiService){ }
+    constructor(private backend: ApiService){ }
 
     getUserId(): string{
         return "a.a@asd.com";
+    }
+    
+    userLogin(email: string, password: string){
+        return this.backend.login(email,password);
+    }
+
+    userSignUp(email: string, firstName: string, lastName: string, password: string){
+        return this.backend.signUp(email,firstName,lastName,password);
     }
 }
