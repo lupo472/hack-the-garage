86597ce84b2b477013d17916a6d11960fea97082
Davide Bonaiuti <davide.bonaiuti92@gmail.com>
2017-02-25 10:46:43 +0100

    report

---
+++ src/app/components/procedure_flow/report.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+
+@Component( {
+    selector: "report",
+    templateUrl: "/app/components/procedure_flow/report.template.html"
+})
+export class ReportComponent {
+
+}
+
+
