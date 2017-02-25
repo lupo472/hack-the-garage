ff22e03cab2b2f8b4c6d73c11e8b6ce2525faabd
Fabio Brunori <fabio.brunori.94@gmail.com>
2017-02-25 15:40:53 +0100

    report now displays descriptions

--- src/app/components/procedure_flow/report.component.ts
+++ src/app/components/procedure_flow/report.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 
 import { ProcedureFlowService } from "../../services/procedure_flow/procedure-flow.service";
+import { ProjectPlanService } from "../../services/procedure_flow/project-plan.service";
 
 import { KeysPipe } from '../../pipes/keys.pipe';
 
@@ -12,14 +13,54 @@ import { KeysPipe } from '../../pipes/keys.pipe';
 export class ReportComponent {
     constructor(
         private flowService:ProcedureFlowService,
+        private planService:ProjectPlanService,
     ) { }
     
-
+    template: any = {};
     allTheData: any = {};
 
     loadData() {
-        //questo viene chiamato 
+        //questo viene chiamato
         this.allTheData = this.flowService.procedure.projectData;
+
+        let proj = this.planService.getProjectTemplate(this.flowService.procedure.projectType.id);
+
+        let navs = {};
+        for (let i = 0; i < proj.length; i++) {
+            let nav = proj[i];
+            let firstLevelObj = {};
+
+            for (let j = 0; j < nav.schede.length; j++) {
+                let scheda = nav.schede[j];
+                let secondLevelObj = {};
+
+                for (let k = 0; k < scheda.sezioni.length; k++) {
+                    let sezione = scheda.sezioni[k];
+                    let thirdLevelObj = {};
+
+                    for (let l = 0; l < sezione.input.length; l++) {
+                        let input = sezione.input[l];
+                        thirdLevelObj[input.key] = {label: input.nome, value: ""};
+                    }
+                    secondLevelObj[sezione.key] = {label: sezione.nome, value: thirdLevelObj};
+                }
+
+                firstLevelObj[scheda.key] = {label: scheda.nome, value: secondLevelObj};
+            }
+
+            navs[nav.key] = {label: nav.nome, value: firstLevelObj};
+        }
+
+        for (let i in navs) {
+            for (let j in navs[i].value) {
+                for (let k in navs[i].value[j].value) {
+                    for (let l in navs[i].value[j].value[k].value) {
+                        navs[i].value[j].value[k].value[l].value = this.allTheData[i][j][k][l];
+                    }
+                }
+            }
+        }
+        this.allTheData = navs;
         console.log(this.allTheData);
 
     }
