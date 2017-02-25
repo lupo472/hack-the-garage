b14b13ef058f24c415073f60c3ee114e2219f2a9
luca <luca.antilici@gmail.com>
2017-02-25 10:43:41 +0100

    Fixed buttons appearing and disappearing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { StatusService } from './services/status.service';
 import { ProjectPlanService } from './services/procedure_flow/project-plan.service';
+import { UserService } from "./services/user.service";
+
 
 @Component({
   selector: 'my-app',
@@ -11,8 +13,14 @@ export class AppComponent  {
   constructor(
       private statusService: StatusService,
       private projectPlanService: ProjectPlanService,
+      private userService: UserService,
     ) { }
   name = 'Angular';
+
+  getUserLogged(){
+    return this.userService.getLoggedIn();
+  }
+  
 
   ngOnInit() {
     
